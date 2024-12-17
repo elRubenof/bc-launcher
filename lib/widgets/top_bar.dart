8c7cff7b4afa07276e209c33748aaf8a7274e86c
elRubenof <elrubenof@gmail.com>
2024-12-17 16:52:54 +0100

    Replaced temporal logo with app logo

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -4,6 +4,7 @@ import 'package:bc_launcher/widgets/tab_button.dart';
 import 'package:bc_launcher/widgets/mouse_icon_button.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class TopBar extends StatefulWidget {
   const TopBar({super.key});
@@ -26,24 +27,40 @@ class _TopBarState extends State<TopBar> {
           MoveWindow(),
           Container(
             height: Constants.topBarHeight,
-            padding: const EdgeInsets.symmetric(horizontal: Constants.horizontalPadding),
+            padding: const EdgeInsets.symmetric(
+              horizontal: Constants.horizontalPadding,
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  height: Constants.topBarHeight,
-                  width: 100,
-                  color: Constants.mainColor,
-                  alignment: Alignment.center,
-                  child: const Text("X",
-                      style: TextStyle(color: Colors.white)), //LOGO PLACEHOLDER
-                ),
+                logo(),
                 tabs(),
                 windowButtons(),
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Widget logo() {
+    if (Constants.appLogo.isEmpty) {
+      return const SizedBox();
+    }
+
+    return Container(
+      height: Constants.topBarHeight,
+      width: 100,
+      color: Constants.mainColor,
+      alignment: Alignment.center,
+      child: SvgPicture.asset(
+        Constants.appLogo,
+        height: Constants.topBarHeight * 0.75,
+        colorFilter: const ColorFilter.mode(
+          Constants.textColor,
+          BlendMode.srcIn,
+        ),
       ),
     );
   }
@@ -76,33 +93,33 @@ class _TopBarState extends State<TopBar> {
       ),
     );
   }
+}
 
-  Widget windowButtons() {
-    return SizedBox(
-      width: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          MouseIconButton(
-            icon: Icons.settings,
-            size: 14.0,
-            backgroundColor: Colors.white.withOpacity(0.12),
-            onTap: () => appWindow.minimize(),
-          ),
-          MouseIconButton(
-            icon: Icons.minimize,
-            size: 14.0,
-            backgroundColor: Colors.white.withOpacity(0.12),
-            onTap: () => appWindow.minimize(),
-          ),
-          MouseIconButton(
-            icon: Icons.close,
-            size: 14.0,
-            backgroundColor: Colors.white.withOpacity(0.12),
-            onTap: () => appWindow.close(),
-          ),
-        ],
-      ),
-    );
-  }
+Widget windowButtons() {
+  return SizedBox(
+    width: 100,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        MouseIconButton(
+          icon: Icons.settings,
+          size: 14.0,
+          backgroundColor: Colors.white,
+          onTap: () => appWindow.minimize(),
+        ),
+        MouseIconButton(
+          icon: Icons.minimize,
+          size: 14.0,
+          backgroundColor: Colors.white,
+          onTap: () => appWindow.minimize(),
+        ),
+        MouseIconButton(
+          icon: Icons.close,
+          size: 14.0,
+          backgroundColor: Colors.white,
+          onTap: () => appWindow.close(),
+        ),
+      ],
+    ),
+  );
 }
